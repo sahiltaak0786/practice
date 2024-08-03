0fc44207d45a3da7285bf79bf1cde985747cd828
Your Name <you@example.com>
2024-08-03 06:19:05 -0700

    First Commit

---
+++ tests/example.spec.js
@@ -0,0 +1,18 @@
+const { test, expect } = require('@playwright/test');
+
+test('Greyt HR ', async ({ page }) => {
+  await page.goto('https://panorama.greythr.com//');
+  await page.locator('[name="username"]').fill('PSS087')
+  await page.locator('[placeholder="Password"]').fill('Sahil123@greythr')
+  await page.locator('[type="submit"]').click();
+  await page.getByRole('button', { name: /Sign/ }).click();
+  await page.locator('[class="toggle"]').first().click();
+  await page.locator('[class="dropdown-item"]').nth(1).click();
+  // await page.locator('gt-button').getByRole('button', { name: 'Sign In' }).nth(1).click();
+  // await page.locator('gt-button').getByRole('button', { name: /Sign/ }).nth(1).click();
+
+  await page.pause();
+
+});
+
+
